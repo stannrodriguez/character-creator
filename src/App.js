3be6aeb836664fd22e4db5f7c8dff5d5ca64aae3
Stephanie Rodriguez <stephsiriban@gmail.com>
2024-10-11 23:26:47 -0400

    add perspective decoration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+
+function BackgroundOverlay() {
+  return (
+    <div
+      style={{
+        background: "hsl(195deg, 10%, 86%)",
+        position: "fixed",
+        bottom: "0px",
+        height: "40%",
+        width: "100%",
+      }}
+    ></div>
+  );
+}
 
 function App() {
   return (
     <>
+      <BackgroundOverlay />
       <CharacterEditor />
       <Footer />
     </>
